a787dd28598d35a8584df701aa740a753bde865d
Benjamin <bemmamin@live.dk>
2022-03-23 11:39:15 +0100

    SerialRead lib and preferences.

--- DispenserProject.ino
+++ DispenserProject.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Stepper.h> // TODO Maybe use accelstepper library
+#include "SerialRead.h"
+#include <Preferences.h>
 
 #define DISPENSERS 3 // Number of snus containers. Odd number only.
 #define STEPS_PER_REVOLUTION 2048 // 32 * 64
@@ -17,7 +19,7 @@ Stepper railStepper(STEPS_PER_REVOLUTION, stepperPins[RAIL_PINS_INDEX][0], stepp
 Stepper actuatorStepper(STEPS_PER_REVOLUTION, stepperPins[ACTUATOR_PINS_INDEX][0], stepperPins[ACTUATOR_PINS_INDEX][1],
                         stepperPins[ACTUATOR_PINS_INDEX][2], stepperPins[ACTUATOR_PINS_INDEX][3]); // Pins
 
-int currentDispenserPosition = DISPENSERS / 2 + 1; // e.g. 3/2=1.5=1+1=2 or 5/2=2.5=2+1=3. 1-indexed. Starts at
+long currentDispenserPosition = DISPENSERS / 2 + 1; // e.g. 3/2=1.5=1+1=2 or 5/2=2.5=2+1=3. 1-indexed. Starts at middle or loaded position.
 
 // Actuator
 int actuatorSteps = 3000;
@@ -25,11 +27,9 @@ int actuatorSteps = 3000;
 // Joystick
 int xyzPins[] = {13, 12, 14};   //x,y,z pins
 
-String inputString;
-
-bool stringComplete;
-
-void checkJoystick();
+// Persistent Storage
+Preferences preferences;
+String positionKey = "position";
 
 void setup() {
     Serial.begin(115200);
@@ -41,36 +41,28 @@ void setup() {
     railStepper.setSpeed(9); // Should be 3-20ms between steps.
     actuatorStepper.setSpeed(9); // Should be 3-20ms between steps.
     pinMode(xyzPins[2], INPUT_PULLUP);  // Z axis is a button (No use for now).
+    preferences.begin("dispenser-app", false);  // Used for saving position.
+    currentDispenserPosition = preferences.getLong(positionKey.c_str(), currentDispenserPosition); // Get stored position or default.
+    Serial.println(currentDispenserPosition);
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        long dispenserIndex = Serial.parseInt(); // Read the integer i.e. dispenser to go to.
+    String serialString = getSerialString();
+
+    // Serial functionality
+    if (serialString.length() != 0) {
+        // Serial is dispenser goto command?
+        long dispenserIndex = serialString.toInt(); // Read the integer i.e. dispenser to go to.
         if (dispenserIndex > 0 && dispenserIndex <= DISPENSERS) {
             Serial.printf("Moving to %ld!\n", dispenserIndex);
             triggerDispenser(dispenserIndex);
+        } else {
+            // Can be used for debugging or testing new features via Serial commands.
+            Serial.println(serialString);
         }
     }
 
-//    if (Serial.available()) {         // judge whether data has been received
-//        char inChar = Serial.read();         // read one character
-//        inputString += inChar;
-//        if (inChar == '\n') {
-//            stringComplete = true;
-//        }
-//    }
-//    if (stringComplete) {
-//        inputString.trim();
-//        // TODO Remove:
-//        if (inputString.equals("forward")) {
-//            railStepper.step(313); // 1000 == 31.3 mm == 313 steps for 1cm ish.
-//        }
-//
-//        inputString = "";
-//        stringComplete = false;
-//    }
-
-    checkJoystick();
+    //checkJoystick();
 }
 
 void checkJoystick() {
@@ -92,6 +84,7 @@ void triggerDispenser(long index) {
         stepsToMove = currentDispenserPosition > index ? -stepsToMove : stepsToMove; // Determine left or right.
         railStepper.step(stepsToMove);
         currentDispenserPosition = index;
+        preferences.putLong(positionKey.c_str(), currentDispenserPosition);
     }
     actuatorStepper.step(-actuatorSteps);
     actuatorStepper.step(actuatorSteps);
---
+++ SerialRead.h
@@ -0,0 +1,7 @@
+#ifndef DISPENSERPROJECT_SERIALREAD_H
+#define DISPENSERPROJECT_SERIALREAD_H
+#include "Arduino.h"
+
+String getSerialString();
+
+#endif //DISPENSERPROJECT_SERIALREAD_H
